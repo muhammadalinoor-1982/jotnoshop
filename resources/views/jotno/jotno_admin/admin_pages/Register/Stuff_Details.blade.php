5d373b10c8dd6f1a64e11eeb1fecabea778c0370
muhammadalinoor-1982 <noor.web.journey@gmail.com>
2022-12-31 16:30:31 +0600

    Customer and Stuff Details page done

--- resources/views/jotno/jotno_admin/admin_pages/Register/Stuff_Details.blade.php
+++ resources/views/jotno/jotno_admin/admin_pages/Register/Stuff_Details.blade.php
@@ -1,73 +1,106 @@
 @extends('jotno.jotno_admin.admin_layout.custom.master')
 @section('content')
-    <div class="row mbn-30">
-        <!--Order Details Head Start-->
-        <div class="col-12 mb-30">
-            <a title="Back to Mail Menu" class="edit button button-box button-xs button-info" href="{{ route('Register.stuff.view')}}"><i class="zmdi zmdi-mail-reply-all"></i></a>
-            <div class="row mbn-180">
-                <div class="box-head">
-                    <h3 class="title" style="position: relative; left: 500px; text-transform: uppercase;">Details of {{ $editData->name }}</h3>
+
+    <div class="row justify-content-between align-items-center mb-10">
+        <!-- Page Headings Start -->
+        <div class="row justify-content-between align-items-center mb-10">
+
+            <!-- Page Heading Start -->
+            <div class="col-12 col-lg-auto mb-20">
+                <div class="page-heading">
+                    <h3><a title="Back to Mail Menu" class="edit button button-box button-xs button-info" href="{{ route('Register.stuff.view')}}"><i class="zmdi zmdi-mail-reply-all"></i></a> &nbsp; Stuff Details Page</h3>
                 </div>
-                <img src="{{(@$editData->image)?url('public/jotno_admin/assets/images/user_images/'.$editData->image):url('public/jotno_admin/assets/images/user_images/noimage.jpg')}}"  alt="" style="position: relative; right: -700px; vertical-align: middle; width: 300px; height: 300px; border-radius: 300%;">
+            </div><!-- Page Heading End -->
 
-            </div>
         </div>
-        <!--Order Details Head End-->
-        @if($editData->status == 'active')
-            <span style="position: relative; right: -300px; top: -275px;" class="badge badge-round badge-success">Active</span><br>
-        @else
-            <span style="position: relative; right: -300px; top: -275px;" class="badge badge-round badge-danger">Inactive</span><br>
-        @endif
-    <!--Order Details Customer Information Start-->
-        <div style="position: relative; top: -300px;" class="col-12 mb-30">
-            <div class="order-details-customer-info row mbn-20">
-                <!--Billing Info Start-->
-                <div style="color: #0bb7ff" class="col-lg-4 col-md-6 col-12 mb-20">
-                    <h4>{{ $editData->name }}</h4>
-                    <span>User Type : </span> <span style="color: yellow"><strong>{{ ucfirst($editData->role) }}</strong> </span><br>
-                    <span>ID : </span> <span>{{ $editData->custom_id }}</span><br>
-                    <span>Email : </span> <span>{{ $editData->email }}</span><br>
-                    <span>Mobile : </span> <span>{{ $editData->mobile }}</span><br>
-                    <span>Nationality : </span> <span>{{ $editData->nationality }}</span><br>
-                    <span>Country : </span> <span>{{ $editData->country }}</span><br>
-                    <span>NID : </span> <span>{{ $editData->nid }}</span><br>
-                    <span>Gender : </span> <span>{{ $editData->gender }}</span><br>
-                    <span>Updater : </span> <span>{{(@$editData->updater)?$editData->updater:"" }}</span>
-                </div>
-                <!--Billing Info End-->
-            </div>
-        </div>
-        <!--Order Details Customer Information Start-->
-
-        {{--<!--Order Details List Start-->
-        <div class="col-12 mb-30">
-            <div class="table-responsive">
-                <table class="table table-bordered table-vertical-middle">
-                    <thead>
-                    <tr>
-                        <th>Product ID</th>
-                        <th>Quantity</th>
-                        <th>Regular Prise</th>
-                        <th>Special Prise</th>
-                        <th>Discount Prise</th>
-                        <th>Bulk Prise</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>--}}{{--{{ $editData->id }}--}}{{--</td>
-                        <td>--}}{{--{{ $editData->quantity }}--}}{{--</td>
-                        <td>--}}{{--{{ $editData->regular_prise }}--}}{{--</td>
-                        <td>--}}{{--{{ $editData->special_prise }}--}}{{--</td>
-                        <td>--}}{{--{{ $editData->discount_prise }}--}}{{--</td>
-                        <td>--}}{{--{{ $editData->bulk_prise }}--}}{{--</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <!--Order Details List End-->--}}
-
+        <!-- Page Headings End -->
     </div>
-    <h4 style="color: #0bb7ff; position: relative; right: -980px; top: -300px;">{{ $editData->name }}</h4>
+
+    <div class="row mbn-50">
+        <!--Timeline / Activities Start-->
+        <div class="col-xlg-12 col-12 mb-50">
+            <div class="box">
+
+                <div class="box-head">
+                    <h3 class="title">Details of {{$editData->name}}</h3>
+                </div>
+
+                <div class="box-body">
+
+                    <div class="timeline-wrap row mbn-50">
+                        <div class="col-12 mb-50">
+                            <ul class="timeline-list">
+                                <li>
+                                    <span class="icon"><i class="zmdi zmdi-receipt"></i></span>
+                                    <div class="details">
+                                        <h5 class="title"><a href="{{ route('Register.stuff.view')}}">{{$editData->name}}</a></h5>
+                                        <div class="gallery">
+                                            <div class="row mbn-30">
+                                                <div class="col-md-4 col-sm-6 col-12 mb-30"><a href="#"><img src="{{(@$editData->image)?url('public/jotno_admin/assets/images/user_images/'.$editData->image):url('public/jotno_admin/assets/images/user_images/noimage.jpg')}}" alt=""></a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <span class="icon"><i class="zmdi zmdi-receipt"></i></span>
+                                    <div class="details">
+                                        <h5 class="title"><a href="{{ route('Register.stuff.view')}}">{{$editData->name}} Details</a></h5>
+                                        <div class="content">
+                                            @if($editData->status == 'active')
+                                                <div class="alert alert-success" role="alert">
+                                                    Status: <a class="alert-link" href="#"> Active</a>
+                                                </div>
+                                            @else
+                                                <div class="alert alert-danger" role="alert">
+                                                    Status: <a class="alert-link" href="#"> Inactive</a>
+                                                </div>
+                                            @endif
+                                            <div class="alert alert-warning" role="alert">
+                                                Role: <a class="alert-link" href="#"> {{ucfirst($editData->role)}}</a>
+                                            </div>
+                                            <div class="alert alert-primary" role="alert">
+                                                ID: <a class="alert-link" href="#"> {{$editData->custom_id}}</a>
+                                            </div>
+                                            <div class="alert alert-primary" role="alert">
+                                                Email: <a class="alert-link" href="#"> {{$editData->email}}</a>
+                                            </div>
+                                            <div class="alert alert-primary" role="alert">
+                                                Mobile:  <a class="alert-link" href="#"> {{$editData->mobile}}</a>
+                                            </div>
+                                            <div class="alert alert-primary" role="alert">
+                                                Nationality: <a class="alert-link" href="#"> {{$editData->nationality}}</a>
+                                            </div>
+                                            <div class="alert alert-primary" role="alert">
+                                                Country: <a class="alert-link" href="#"> {{$editData->country}}</a>
+                                            </div>
+                                            <div class="alert alert-primary" role="alert">
+                                                NID: <a class="alert-link" href="#"> {{$editData->nid}}</a>
+                                            </div>
+                                            <div class="alert alert-primary" role="alert">
+                                                Gender: <a class="alert-link" href="#"> {{$editData->gender}}</a>
+                                            </div>
+                                            @if($editData->updater)
+                                                <div class="alert alert-primary" role="alert">
+                                                    Updater: <a class="alert-link" href="#"> {{$editData->updater}}</a>
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <!--Timeline / Activities End-->
+    </div>
+
 @endsection
+
+
+
